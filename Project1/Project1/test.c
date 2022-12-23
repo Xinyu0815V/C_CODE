330a1b9ebada1a4bc829647d646831ff543a7ec2
xinyu <T2693499583@163.com>
2022-12-23 19:58:21 +0800

    测试github代码

---
+++ Project1/Project1/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	//printf("你好，田新宇");
+	int a = 0;
+	printf("请输入数字：");
+	scanf("%d", &a);
+	printf("a = %d\n", a);
+	return 0;
+}
